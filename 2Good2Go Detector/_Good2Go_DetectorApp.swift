04b6e6d6e9f74c79868e7a6fb41494642c7c6529
Markus Ebner <ebner.markus33@t-online.de>
2024-11-12 19:35:02 +0100

    Initial Commit

---
+++ 2Good2Go Detector/_Good2Go_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Good2Go_DetectorApp.swift
+//  2Good2Go Detector
+//
+//  Created by Anna B. on 12.11.24.
+//
+
+import SwiftUI
+
+@main
+struct _Good2Go_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
